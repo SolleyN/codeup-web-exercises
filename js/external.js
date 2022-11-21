156db8836a415065166a7af4a7447925bb83dbb8
Nickolas Solley <nicksolley@gmail.com>
2022-11-21 13:37:53 -0600

    uploading first exercises

---
+++ js/external.js
@@ -0,0 +1,7 @@
+alert("Welcome to my website!");
+
+let favColor = prompt('What is your favorite color?');
+let part1 = "Great, ";
+let part2 = " is my favorite color, too!";
+let message = part1 + favColor + part2;
+alert(message);
