527aea4f1b2d20d36f25904e4fe8f22f2070c6e0
Nickolas Solley <nicksolley@gmail.com>
2022-11-21 20:06:47 -0600

    Working on conditional exercises.

---
+++ js/conditionals.js
@@ -0,0 +1,75 @@
+"use strict";
+
+/* ########################################################################## */
+
+/**
+ * TODO:
+ * Create a function named `analyzeColor` that accepts a string that is a color
+ * name as input. This function should return a message which relates to the
+ * color stated in the argument of the function. For colors you do not have
+ * responses written for, return a string stating so
+ *
+ * Example:
+ *  > analyzeColor('blue') // returns "blue is the color of the sky"
+ *  > analyzeColor('red') // returns "Strawberries are red"
+ *
+ *
+ *  > analyzeColor('cyan') // returns "I don't know anything about cyan"
+ *
+ * You should use an if-else-if-else block to return different messages.
+ *
+ * Test your function by passing various string literals to it and
+ * console.logging the function's return value
+ */
+function analyzeColor(colorName) {
+    let colorName = prompt("What is your color name?")
+
+    if (colorName === "red");
+    {
+        return "Red is the color of a fire hydrant.";
+    }
+    else if (colorName === "blue");
+
+        return "blue is the color of the ocean.";
+} else if
+    {
+        colorName === "green";
+    return "My girlfriend's favorite color is green."
+}
+else {
+    return analyzeColor("I don't know anything about" + (colorName) ;
+
+}
+
+console.log(colorName);
+
+
+
+
+// Don't change the next two lines!
+// These lines create two variables for you:
+// - `colors`: a list of the colors of the rainbow
+// - `randomColor`: contains a single random color value from the list (this
+//                  will contain a different color every time the page loads)
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var randomColor = colors[Math.floor(Math.random() * colors.length)];
+/**
+ * TODO:
+ * Pass the `randomColor` variable to your 'analyzeColor' function and console.log the results.
+ * You should see a different message every time you refresh the page
+ */
+analyzeColor(randomColor);
+
+console.log(randomColor);
+
+/**
+ * TODO:
+ * Comment out the code above, and refactor your function to use a switch-case statement
+ */
+
+/**
+ * TODO:
+ * Prompt the user for a color when the page loads, and pass the input from the
+ * user to your `analyzeColor` function. Alert the return value from your
+ * function to show it to the user.
+ */
