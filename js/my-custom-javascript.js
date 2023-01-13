7da2ed228d3eaa9f894d1e51b59ad4f384a7b71c
Nickolas Solley <nicksolley@gmail.com>
2023-01-13 16:02:51 -0600

    Finished JqUery Excercises

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,11 +1,23 @@
-"use strict";
+"use strict"
 
-$(function() {
+// $(function() {
 
-    // INSERT JAVASCRIPT CODE SAMPLE HERE
-
-    $(function() {
-        alert( 'The DOM has finished loading!' );
-    });
+//     $(function() {
+//         alert( 'The DOM has finished loading!' );
+//     });
     
-});
+// });
+
+// var contents = $('#codeup').html();
+// alert(contents);
+
+
+// let contents = $('#p1').html();
+// alert(contents);
+
+// $('.codeup').css('background-color', 'red');
+
+// $('.codeup').css( 'border', 'solid 1px red');
+
+//  let contents = $('li').css('font-size', '20px').html();
+let contents = $('h1, p, li').css('background-color', 'yellow').html();
