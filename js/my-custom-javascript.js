6a7382425125059aff9b9e77707c966c567b50ee
Nickolas Solley <nicksolley@gmail.com>
2023-01-17 12:07:38 -0600

    jQuery mouse click events

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,17 +1,14 @@
 "use strict"
 
-// $(function() {
+$(document).ready(function() {
 
 //     $(function() {
 //         alert( 'The DOM has finished loading!' );
 //     });
     
 // });
-
 // var contents = $('#codeup').html();
 // alert(contents);
-
-
 // let contents = $('#p1').html();
 // alert(contents);
 
@@ -20,4 +17,26 @@
 // $('.codeup').css( 'border', 'solid 1px red');
 
 //  let contents = $('li').css('font-size', '20px').html();
-let contents = $('h1, p, li').css('background-color', 'yellow').html();
+// let contents = $('h1, p, li').css('background-color', 'yellow').html();
+
+// $('#codeup').click(function() {
+//     alert('h1 with id "codeup" was clicked');
+
+$('#title').click(function() {
+    $(this).css('background-color', 'red');
+});
+
+$('#p1').dblclick(function() {
+    $(this).css ('font-size', '18px');
+});
+
+$('li').hover(function() {
+
+$('li').css('color', 'red');
+
+}, function(){
+
+    $('li').css('color', '');
+    
+})
+});
