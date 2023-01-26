844bca5e65b63aae9dbe6b371deea0d1bf2a1f3e
Nickolas Solley <nicksolley@gmail.com>
2023-01-26 16:10:24 -0600

    styling

--- js/weather_map.js
+++ js/weather_map.js
@@ -2,6 +2,7 @@
 "use strict";
 
 //mapbox js
+
 mapboxgl.accessToken = keys.mapbox;
 const map = new mapboxgl.Map({
 container: 'map', 
@@ -10,10 +11,13 @@ center: [-98.4916, 29.4252],
 zoom: 1.8, 
 projection: "globe",
 });
+
 map.on('style.load', () => {
+
     // Custom atmosphere styling
+
     map.setFog({
-    'color': 'rgb(52, 186, 235)', // Pink fog 
+    'color': 'rgb(52, 186, 235)', // blue fog 
     'high-color': 'rgb(36, 92, 223)', // Blue sky 
     'horizon-blend': 0.1 // Exaggerate atmosphere 
     });
@@ -27,6 +31,7 @@ map.on('style.load', () => {
     'source': 'mapbox-dem',
     'exaggeration': 1.5
     });
+
     // drops marker on san antonio
     });
 let marker = new mapboxgl.Marker({
@@ -40,8 +45,6 @@ function onDragEnd() {
     
 }
 
-
-
 // grabs user coordinates and stores them in userLocation
 $(`#search`).on(`click`, function(){
 let userLocation = $(`#location`).val();
@@ -50,10 +53,8 @@ geocode(userLocation, keys.mapbox).then(function (result){
   map.setZoom(9);
 
  
-  
- 
-  
-   // calls the weather api 
+// calls the weather api 
+
   $.get('https://api.openweathermap.org/data/2.5/forecast', {
       lat: result[1],
       lon: result[0],
@@ -61,13 +62,15 @@ geocode(userLocation, keys.mapbox).then(function (result){
       units: 'imperial'
 
   }).done(function(data){
+
       // displays weather of selected location and displays in cards
+      
       let html = '';
       for (let i = 0; i < data.list.length; i += 8) {
         
           html += `<div class="card m-3 bg-dark text-light border border-secondary">
           <div class="card-header">
-          <div>${data.list[i].dt_txt.slice(0,10)}</div>
+          <div>${data.list[i].dt_txt.slice(0, 10)}</div>
           </div>
       <ul class="list-group ">
           <li class="list-group-item --bs-info-text text-dark">High: ${parseInt(data.list[i].main.temp_max)} Low: ${parseInt(data.list[i].main.temp_min)}</li>
